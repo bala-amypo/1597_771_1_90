615b3ece6f86979e0b7bced6ffa23990e3006e78
bala-amypo <bala@amypo.in>
2025-12-19 14:10:13 +0000

    User commit

--- src/main/java/com/example/demo/Service/impl/TicketCategoryServiceImpl.java
+++ src/main/java/com/example/demo/Service/impl/TicketCategoryServiceImpl.java
@@ -1,22 +1,22 @@
-// @Service
-// @RequiredConstructor
-// public class TicketCategoryServiceImpl implements TicketCategoryService {
+@Service
+@RequiredConstructor
+public class TicketCategoryServiceImpl implements TicketCategoryService {
 
-//     private final TicketCategoryRepository categoryRepository;
+    private final TicketCategoryRepository categoryRepository;
 
-//     @Override
-//     public TicketCategory createCategory(TicketCategory category) {
-//         return categoryRepository.save(category);
-//     }
+    @Override
+    public TicketCategory createCategory(TicketCategory category) {
+        return categoryRepository.save(category);
+    }
 
-//     @Override
-//     public List<TicketCategory> getAllCategories() {
-//         return categoryRepository.findAll();
-//     }
+    @Override
+    public List<TicketCategory> getAllCategories() {
+        return categoryRepository.findAll();
+    }
 
-//     @Override
-//     public TicketCategory getCategory(Long id) {
-//         return categoryRepository.findById(id)
-//                 .orElseThrow(() -> new RuntimeException("Category not found with id: " + id));
-//     }
-// }
+    @Override
+    public TicketCategory getCategory(Long id) {
+        return categoryRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Category not found with id: " + id));
+    }
+}
