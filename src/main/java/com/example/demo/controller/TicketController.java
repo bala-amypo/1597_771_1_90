f12b11d22c7d446fe175e77ca8cfb142789fc3f0
bala-amypo <bala@amypo.in>
2025-12-19 13:37:13 +0000

    User commit

--- src/main/java/com/example/demo/controller/TicketController.java
+++ src/main/java/com/example/demo/controller/TicketController.java
@@ -1,44 +1,44 @@
-// import java.util.List;
+import java.util.List;
 
-// import org.springframework.stereotype.Service;
-// import org.springframework.web.bind.annotation.RestController;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.http.ResponseEntity;
-
-
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.ResponseEntity;
 
 
-// @RestController
-// @RequestMapping("/api/tickets")
-// @Tag(name = "Tickets", description = "Ticket management APIs")
-// @RequiredArgsConstructor
-// public class TicketController {
 
-//     private final TicketService ticketService;
 
-//     @PostMapping("/{userId}/{categoryId}")
-//     public ResponseEntity<Ticket> createTicket(
-//             @PathVariable Long userId,
-//             @PathVariable Long categoryId,
-//             @RequestBody Ticket ticket) {
+@RestController
+@RequestMapping("/api/tickets")
+@Tag(name = "Tickets", description = "Ticket management APIs")
+@RequiredArgsConstructor
+public class TicketController {
 
-//         return ResponseEntity.ok(
-//                 ticketService.createTicket(userId, categoryId, ticket)
-//         );
-//     }
+    private final TicketService ticketService;
 
-//     @GetMapping("/user/{userId}")
-//     public ResponseEntity<List<Ticket>> getByUser(@PathVariable Long userId) {
-//         return ResponseEntity.ok(ticketService.getTicketsByUser(userId));
-//     }
+    @PostMapping("/{userId}/{categoryId}")
+    public ResponseEntity<Ticket> createTicket(
+            @PathVariable Long userId,
+            @PathVariable Long categoryId,
+            @RequestBody Ticket ticket) {
 
-//     @GetMapping("/all")
-//     public ResponseEntity<List<Ticket>> getAll() {
-//         return ResponseEntity.ok(ticketService.getAllTickets());
-//     }
+        return ResponseEntity.ok(
+                ticketService.createTicket(userId, categoryId, ticket)
+        );
+    }
 
-//     @GetMapping("/{id}")
-//     public ResponseEntity<Ticket> getById(@PathVariable Long id) {
-//         return ResponseEntity.ok(ticketService.getTicket(id));
-//     }
-// }
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<Ticket>> getByUser(@PathVariable Long userId) {
+        return ResponseEntity.ok(ticketService.getTicketsByUser(userId));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Ticket>> getAll() {
+        return ResponseEntity.ok(ticketService.getAllTickets());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Ticket> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(ticketService.getTicket(id));
+    }
+}
