0b1fe79891793b203288be04bcab67d2aa3e2305
bala-amypo <bala@amypo.in>
2025-12-19 04:15:11 +0000

    User commit

---
+++ src/main/java/com/Service/UserService.java
@@ -0,0 +1,8 @@
+public interface UserService {
+
+    User registerUser(User user);
+
+    User getUser(Long id);
+
+    List<User> getAllUsers();
+}
---
+++ src/main/java/com/Service/impl/UserServiceImpl.java
@@ -0,0 +1,22 @@
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public User registerUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User getUser(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found with id: " + id));
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
