938b3f231bb0151c25161837c6455ff87019ec3b
bala-amypo <bala@amypo.in>
2025-12-21 16:19:01 +0000

    User commit

--- src/main/java/com/example/demo/repositories/TicketCategoryRepository.java
+++ src/main/java/com/example/demo/repositories/TicketCategoryRepository.java
@@ -1,9 +1,9 @@
-// package com.example.demo.repository;
+package com.example.demo.repository;
 
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import com.example.demo.model.TicketCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.model.TicketCategory;
 
-// public interface TicketCategoryRepository extends JpaRepository<TicketCategory, Long> {
+public interface TicketCategoryRepository extends JpaRepository<TicketCategory, Long> {
 
-//     boolean existsByCategoryName(String categoryName);
-// }
+    boolean existsByCategoryName(String categoryName);
+}
