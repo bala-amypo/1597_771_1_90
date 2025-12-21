5dc11b906b134ccf973e23b9ed2cd7becd79f75c
bala-amypo <bala@amypo.in>
2025-12-21 18:00:31 +0000

    User commit

--- src/main/java/com/example/demo/repositories/TicketRepository.java
+++ src/main/java/com/example/demo/repositories/TicketRepository.java
@@ -1,13 +1,12 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.Ticket;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.model.Ticket;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
 
-    List<Ticket> findByUser_Id(Long userId);
-
     List<Ticket> findByStatus(String status);
+
+    List<Ticket> findByUser_Id(Long userId);
 }
