a72460f9238314b67b80d2df626861eafe8f95b4
bala-amypo <bala@amypo.in>
2025-12-26 04:46:41 +0000

    User commit

--- src/main/java/com/example/demo/repository     /TicketCategoryRepository.java
+++ src/main/java/com/example/demo/repository     /TicketCategoryRepository.java
@@ -2,6 +2,9 @@ package com.example.demo.repository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.TicketCategory;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 @Repository
 public interface TicketCategoryRepository extends JpaRepository<TicketCategory, Long> {
 
