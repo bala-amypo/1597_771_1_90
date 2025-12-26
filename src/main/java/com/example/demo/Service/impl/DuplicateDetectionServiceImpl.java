61bfce52ddf2d2137fde7789997ef99cfb43f55c
bala-amypo <bala@amypo.in>
2025-12-26 04:44:27 +0000

    User commit

--- src/main/java/com/example/demo/Service/impl/DuplicateDetectionServiceImpl.java
+++ src/main/java/com/example/demo/Service/impl/DuplicateDetectionServiceImpl.java
@@ -1,13 +1,15 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.model.DuplicateDetectionLog;
-import com.example.demo.model.Ticket;
+import com.example.demo.repository.TicketRepository;
+import com.example.demo.repository.DuplicateRuleRepository;
 import com.example.demo.repository.DuplicateDetectionLogRepository;
 import com.example.demo.service.DuplicateDetectionService;
-import lombok.RequiredArgsConstructor;
+import com.example.demo.model.DuplicateDetectionLog;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+
 @Service
 public class DuplicateDetectionServiceImpl implements DuplicateDetectionService {
 
--- src/main/java/com/example/demo/repositories/DuplicateDetectionLogRepository.java
+++ src/main/java/com/example/demo/repositories/DuplicateDetectionLogRepository.java
@@ -2,6 +2,8 @@ package com.example.demo.repository;
 
 import com.example.demo.model.DuplicateDetectionLog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 @Repository
