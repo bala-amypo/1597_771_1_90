b53d0337e5e9f98c610f1bbb31739460814545bd
bala-amypo <bala@amypo.in>
2025-12-26 09:10:30 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -3,8 +3,6 @@ package com.example.demo.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,20 +11,13 @@ import org.springframework.context.annotation.Configuration;
         info = @Info(
                 title = "Complaint Duplicate Detector API",
                 version = "1.0",
-                description = "API documentation for Complaint Duplicate Detector"
+                description = "API documentation"
         )
 )
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new io.swagger.v3.oas.models.info.Info()
-                        .title("Complaint Duplicate Detector API")
-                        .version("1.0")
-                        .description("API documentation")
-                        .contact(new Contact().name("Support"))
-                        .license(new License().name("Apache 2.0"))
-                );
+        return new OpenAPI();
     }
 }
