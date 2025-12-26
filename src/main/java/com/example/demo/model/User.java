544850ab428b266d531b0d572529f5e490d51d2f
bala-amypo <bala@amypo.in>
2025-12-26 10:09:40 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -37,7 +37,7 @@ public class User {
         this.fullName = fullName;
         this.email = email;
         this.password = password;
-        this.role = (role == null) ? "USER" : role;
+        this.role = role;
     }
 
     @PrePersist
@@ -56,43 +56,19 @@ public class User {
         return fullName;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
     public String getEmail() {
         return email;
     }
-    
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    
+
     public String getPassword() {
         return password;
     }
-    
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    
+
     public String getRole() {
         return role;
-    }
-    
-    public void setRole(String role) {
-        this.role = role;
     }
 
     public LocalDateTime getCreatedAt() {
         return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
     }
 }
